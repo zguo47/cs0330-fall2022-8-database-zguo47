04ea92c564d49a46c298ca7393a88ef400a1afb6
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2022-12-10 00:10:00 -0500

    New

--- server.c
+++ server.c
@@ -164,7 +164,7 @@ void *run_client(void *arg) {
         interpret_command(command, response, strlen(response));
     }
 
-    // pthread_exit(0);
+    pthread_exit(new_client->thread);
     client_destructor(new_client);
     // pthread_cleanup_pop();
 }
